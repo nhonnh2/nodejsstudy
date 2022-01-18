14f11f03e7ae8ff9ea3c72cd0d9a54dab4be9f7f
Nguyen Huu Nhon <huunhon099@gmail.com>
2022-01-18 23:43:45 +0700

    delete count

--- src/app/controller/MeController.js
+++ src/app/controller/MeController.js
@@ -2,12 +2,23 @@ const Course = require('../model/Course');
 const { mutipleMongooseToObject } = require('../../util/mongoose');
 class Mecontroller {
     storedCourses(req, res, next) {
-        Course.find().then((courses) => {
-            res.render('me/stored-course', {
-                title: 'khoa hoc cua toi',
-                courses: mutipleMongooseToObject(courses),
-            });
-        });
+        //2 thao tác bất đồng bộ xảy ra cùng lúc nên ta dùng promise.all
+        Promise.all([Course.countDocumentsDeleted(), Course.find()])
+            .then(([deleteCount, courses]) => {
+                res.render('me/stored-course', {
+                    title: 'khoa hoc cua toi',
+                    courses: mutipleMongooseToObject(courses),
+                    deleteCount
+                });
+            })
+
+        // Course.countDocumentsDeleted().then((count) => console.log(count))
+        // Course.find().then((courses) => {
+        //     res.render('me/stored-course', {
+        //         title: 'khoa hoc cua toi',
+        //         courses: mutipleMongooseToObject(courses),
+        //     });
+        // });
     }
     trashCourses(req, res, next) {
         Course.findDeleted().then((courses) => {
